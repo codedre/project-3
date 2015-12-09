d637017e75147f7f71580d99ba9e496e3a7dbd58
Tyler <tylerscottcrosse@gmail.com>
2015-12-09 17:03:31 -0500

    Add funciton to show all in filter

--- public/js/views/userView.js
+++ public/js/views/userView.js
@@ -11,15 +11,16 @@ var UserView = function(user) {
 
   $('#filterLocation').change(function() {
     var val = $('#filterLocation option:selected').val();
-    console.log(val);
-
     var $cards = $(".card-text");
 
     $cards.each(function(index, el){
       $el = $(el);
-      if ($el.text() != val) {
-        console.log($el);
+      if (val === 'All') {
+        $el.parents(':eq(1)').show();
+      } else if ($el.text() != val) {
         $el.parents(':eq(1)').hide();
+      } else if ($el.text() == val) {
+        $el.parents(':eq(1)').show();
       }
     });
   });
@@ -68,12 +69,13 @@ UserView.prototype = {
     html.append('<p>Interested in <a class="btn btn-warning" href="#">Foodie</a> <a class="btn btn-warning" href="#">Wildlife</a> <a class="btn btn-default" href="#">+ Add Interest</a></p>');
     var filterGroup = $('<div id="filterGroup"></div>');
     var sel = $('<select class="btn btn-warning" id="filterLocation"></select>');
+    sel.append('<option name="all">All</option>');
     sel.append('<option name="northWest">North West</option>');
     sel.append('<option name="west">West</option>');
     sel.append('<option name="southWest">South West</option>');
     sel.append('<option name="mid-west">Mid-West</option>');
     sel.append('<option name="southEast">South East</option>');
-    sel.append('<option name="mid-atlantic">mid-atlantic</option>');
+    sel.append('<option name="mid-atlantic">Mid-Atlantic</option>');
     sel.append('<option name="northEast">North East</option>');
     filterGroup.append(sel);
     html.append(filterGroup);

eb688e05ed457b3aeffb1e8df0164756f12cfa48
Tyler <tylerscottcrosse@gmail.com>
2015-12-08 19:19:20 -0500

    Working basic renderUsers function

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  $("body").css({
-    "background"            : "no-repeat",
-    "background-image"      : "url(images/bg_home.jpg)",
-    "background-position"   : "center center",
-    "background-attachment" : "fixed",
-    "background-size"       : "cover"
+  // $("body").css({
+  //   "background"            : "no-repeat",
+  //   "background-image"      : "url(images/bg_home.jpg)",
+  //   "background-position"   : "center center",
+  //   "background-attachment" : "fixed",
+  //   "background-size"       : "cover"
+  // });
+
+  User.fetch().then(function(users){
+    users.forEach( function(user) {
+      var view = new UserView(user).renderUsers();
+    });
   });
+
 });
--- public/js/views/userView.js
+++ public/js/views/userView.js
@@ -1,11 +1,14 @@
 var UserView = function(user) {
   this.user = user;
-  this.$el = $('<div class="user></div>');
+  this.$el = $('<div class="user"></div>');
 };
 
 UserView.prototype = {
   renderUsers: function() {
     var self = this;
+    self.$el.html(self.userTemplate(self.user));
+
+    $("body").append(self.$el);
 
 
 
@@ -24,4 +27,13 @@ UserView.prototype = {
     // have search as static form with dropdowns on page, on submit
     // render apprpriate users
   },
+  userTemplate: function(user) {
+    var html = $("<div>");
+    html.append("<h2>" + user.name + "</h2>");
+    // html.append("<img class='artist-photo' src='" + artist.photoUrl + "'>");
+    // html.append("<button class='showSongs'>Show Songs</button>");
+    // html.append("<button class='editArtist'>Edit Artist</button>");
+    // html.append("<div class='songs'></div>");
+    return(html);
+  }
 };

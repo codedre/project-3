e608b4660326d86efa776700de4c130bc6bf9001
ANDRE SHONUBI <andre@codedre.com>
2015-12-10 15:17:21 -0500

    fingers crossed

--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,7 @@
 require("./schema");
 var mongoose = require("mongoose");
-var db = mongoose.createConnection(process.env.MONGOLAB_URI || 'mongodb://localhost/beacon');
- // mongoose.connection;
+var conn = mongoose.createConnection(process.env.MONGOLAB_URI || 'mongodb://localhost/beacon');
+var db = mongoose.connection;
 var userData = require("./user_data");
 
 

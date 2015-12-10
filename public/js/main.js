715065eb392a1c518142ba69377a7049dc6d64bf
Tyler <tylerscottcrosse@gmail.com>
2015-12-10 15:22:32 -0500

    Clean up main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,6 @@ $(document).ready(function() {
         loadUserIndexView();
       } else {
         var background = new WelcomeView().renderBackground();
-        // $("body").css({
-        //   "background"            : "no-repeat",
-        //   "background-image"      : "url(images/bg_home.jpg)",
-        //   "background-position"   : "center center",
-        //   "background-attachment" : "fixed",
-        //   "background-size"       : "cover"
-        // });
       }
     });
 
